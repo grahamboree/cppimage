00dd0699717b9952118741c00ef7171033ead193
Graham Pentheny <graham.pentheny@gmail.com>
2012-11-07 23:56:49 -0500

    gtest added with some minor indenting adjustments

---
+++ cppimage_tests/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cppimage_tests
+//
+//  Created by Graham Pentheny on 11/7/12.
+//  Copyright (c) 2012 Graham Pentheny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
